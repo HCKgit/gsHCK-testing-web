b7ea2fd79d8e27597531b5e2b51a34272cf38265
HCKgit <hckilburn@gmail.com>
2018-06-14 14:48:39 -0300

    Fix found in Section 87 of manual. https://docs.spring.io/spring-boot/docs/current/reference/html/howto-traditional-deployment.html

--- complete/src/main/java/hello/GreetingService.java
+++ complete/src/main/java/hello/GreetingService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
     public String greet() {
-        return "Hello World";
+        return "Hello World greeting service (from a warfile even)";
     }
 }
+
